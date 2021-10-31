4ee90fa706f8cc3cf147fe1300bc22487cdb538a
Danya <ermakov1709@mail.ru>
2021-10-31 22:45:14 +0300

    Добавьте файлы проекта.

---
+++ OS_KP/FileWork.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace OS_KP
+{
+    class FileWork
+    {
+        public static void OpenFile(string path, out string pages, out int memoryCount)
+        {
+            string str = null;
+            str = System.IO.File.ReadAllText(path);
+
+
+            string[] stringSeparators = { "\r", "\n", " ", "\t" };
+            List<int> tempList = str.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(n => int.Parse(n))
+                                    .ToList();
+            memoryCount = tempList[0];
+            tempList.RemoveAt(0);
+            pages = string.Join(" ", tempList);
+            path = string.Empty;
+        }
+
+        public static void CreatePDF(string path, byte[] bitmap, string nruLog, string lruLog, string fifoLog, string scLog, string clockLog )
+        {
+            string lucidaConsolePath = "../../resources/lucidaConsole.ttf";
+            BaseFont bf1 = BaseFont.CreateFont(lucidaConsolePath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            Font lucidaConsole = new Font(bf1, 10, Font.NORMAL);
+
+            string timesNewRomanPath = "../../resources/timesNewRoman.ttf";
+            BaseFont bf2 = BaseFont.CreateFont(timesNewRomanPath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            Font timesNewRoman = new Font(bf2, 20, Font.BOLD);
+
+            Document doc = new Document(PageSize.A4);
+            var writer = PdfWriter.GetInstance(doc, new FileStream(path, FileMode.Create)); 
+            doc.Open();
+
+            var header = new Paragraph("Анализ методов замещения страниц\n", timesNewRoman);
+            header.Alignment = Element.ALIGN_CENTER;
+            doc.Add(header);
+
+            var image = iTextSharp.text.Image.GetInstance(bitmap);
+
+            image.Alignment = Element.ALIGN_CENTER;
+            doc.Add(image);
+
+            //var nruLogParagraph = new Paragraph(nruLog + "\n", FontFactory.GetFont("LUCIDA_CONSOLE", Encoding.UTF8.ToString(), 12));
+            var nruLogParagraph = new Paragraph(nruLog + "\n\n", lucidaConsole);
+            nruLogParagraph.Alignment = Element.ALIGN_JUSTIFIED;
+            doc.Add(nruLogParagraph);
+
+            //var lruLogParagraph = new Paragraph(lruLog + "\n", FontFactory.GetFont("LUCIDA_CONSOLE", 12));
+            var lruLogParagraph = new Paragraph(lruLog + "\n\n", lucidaConsole);
+            lruLogParagraph.Alignment = Element.ALIGN_JUSTIFIED;
+            doc.Add(lruLogParagraph);
+
+            //var fifoLogParagraph = new Paragraph(fifoLog + "\n", FontFactory.GetFont("LUCIDA_CONSOLE", 12));
+            var fifoLogParagraph = new Paragraph(fifoLog + "\n\n", lucidaConsole);
+            fifoLogParagraph.Alignment = Element.ALIGN_JUSTIFIED;
+            doc.Add(fifoLogParagraph);
+
+            //var scLogParagraph = new Paragraph(scLog + "\n", FontFactory.GetFont("LUCIDA_CONSOLE", 12));
+            var scLogParagraph = new Paragraph(scLog + "\n\n", lucidaConsole);
+            scLogParagraph.Alignment = Element.ALIGN_JUSTIFIED;
+            doc.Add(scLogParagraph);
+
+            //var clockLogParagraph = new Paragraph(clockLog + "\n", FontFactory.GetFont("LUCIDA_CONSOLE", 12));
+            var clockLogParagraph = new Paragraph(clockLog + "\n\n", lucidaConsole);
+            clockLogParagraph.Alignment = Element.ALIGN_JUSTIFIED;
+            doc.Add(clockLogParagraph);
+
+            doc.Close();
+        }
+    }
+}
